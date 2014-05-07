0dc8ed08a7ec05f6da695bf7d2ad744627b49db6
AVRoosin <avroosin@mail.ru>
2014-05-07 15:39:27 +0600

    apply pg util

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -8,6 +7,23 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+
+var pg = require('pg');
+var conString = "postgres://dzwbxhpjsrzcri:B1_C7gHTzGm8qkzRX0wnU6tixR@ec2-54-225-239-184.compute-1.amazonaws.com:5432/d2vm1254u3rh9f";
+var client = new pg.Client(conString);
+client.connect(function (err) {
+    if (err) {
+        return console.error('could not connect to postgres', err);
+    }
+    client.query('SELECT NOW() AS "theTime"', function (err, result) {
+        if (err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].theTime);
+        //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+        client.end();
+    });
+});
 
 var app = express();
 // all environments
@@ -26,12 +42,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
