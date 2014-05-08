b96c5422f3bd9275179ccd34c7a19f2d148d35e0
AVRoosin <avroosin@mail.ru>
2014-05-08 14:23:09 +0600

    first query

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,22 @@ client.connect(function (err) {
     if (err) {
         return console.error('could not connect to postgres', err);
     }
-    client.query('SELECT NOW() AS "theTime"', function (err, result) {
+//    client.query('SELECT NOW() AS "theTime"', function (err, result) {
+//        if (err) {
+//            return console.error('error running query', err);
+//        }
+//        cons = result.rows[0].theTime;
+//        //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+//        client.end();
+//    });
+    client.query('SELECT * FROM "PartsOfSpeech" AS "theParts"', function (err, result) {
         if (err) {
-            return console.error('error running query', err);
+            cons = "error running query";
+            return;
         }
-        cons = result.rows[0].theTime;
-        //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+        cons = result.rows[0].theParts;
         client.end();
-    });
+    })
 });
 
 
