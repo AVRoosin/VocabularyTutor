c330d0ed923253e489d93bc07973807f768dcf6f
AVRoosin <avroosin@mail.ru>
2014-06-14 21:56:33 +0600

    added templating with ejs-locals

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ var http = require('http');
 var path = require('path');
 var config = require('config');
 var log = require('libs/log')(module);
+var engine=require('ejs-locals');
 
 var app = express();
 // all environments
 app.set('port', process.env.PORT || config.get('port'));
-app.set('views', path.join(__dirname, 'views'));
+
+app.engine('ejs', engine); // layout partial block
+app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -32,6 +36,21 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+//libs & styles
+//app.get('/public/css/style.css', function (req, res, next) {
+//    res.sendfile('style.css');
+//});
+app.get('./public/vendor/bower_components/bootstrap/dist/js/bootstrap.min.js', function (req, res, next) {
+    res.sendfile('style.css');
+});
+app.get('./public/vendor/bower_components/jquery/dist/jquery.min.js', function (req, res, next) {
+    res.sendfile('jquery.js');
+});
+app.get('./public/vendor/bower_components/bootstrap/dist/css/bootstrap.min.css', function (req, res, next) {
+    res.sendfile('bootstrap.min.css');
+});
+
 
 http.createServer(app).listen(app.get('port'), function () {
     log.info('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,35 @@
  * GET home page.
  */
 
-var cons = '';
-var pg = require('pg');
-var params = { host: 'ec2-54-225-239-184.compute-1.amazonaws.com', port: '5432', user: 'dzwbxhpjsrzcri', password: 'B1_C7gHTzGm8qkzRX0wnU6tixR', database: 'd2vm1254u3rh9f', ssl: true };
-var client = new pg.Client(params);
-client.connect(function (err) {
-    if (err) {
-        return console.error('could not connect to postgres', err);
-    }
-    client.query('SELECT NOW() AS "theTime"', function (err, result) {
-        if (err) {
-            return console.error('error running query', err);
-        }
-        cons = result.rows[0].theTime;
-        //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-        client.end();
-    });
-//    client.query('SELECT * FROM "PartsOfSpeech" AS "theParts"', function (err, result) {
+//var cons = '';
+//var pg = require('pg');
+//var params = { host: 'ec2-54-225-239-184.compute-1.amazonaws.com', port: '5432', user: 'dzwbxhpjsrzcri', password: 'B1_C7gHTzGm8qkzRX0wnU6tixR', database: 'd2vm1254u3rh9f', ssl: true };
+//var client = new pg.Client(params);
+//client.connect(function (err) {
+//    if (err) {
+//        return console.error('could not connect to postgres', err);
+//    }
+//    client.query('SELECT NOW() AS "theTime"', function (err, result) {
 //        if (err) {
-//            cons = "error running query";
+//            return console.error('error running query', err);
 //        }
-//        cons = result.rows[0].PartOfSpeech_Name;
-//        console.log(result.rows[0].PartOfSpeech_Name);
+//        cons = result.rows[0].theTime;
+//        //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
 //        client.end();
 //    });
-});
+////    client.query('SELECT * FROM "PartsOfSpeech" AS "theParts"', function (err, result) {
+////        if (err) {
+////            cons = "error running query";
+////        }
+////        cons = result.rows[0].PartOfSpeech_Name;
+////        console.log(result.rows[0].PartOfSpeech_Name);
+////        client.end();
+////    });
+//});
 
 
-exports.index = function (req, res) {
-    res.render('index', { title: 'Vocabulary Tutor' + cons });
-    console. log(cons);
+exports.index = function (req, res, next) {
+    res.render('index', { title: 'Vocabulary Tutor', muppets: [ 'Kermit', 'Fozzie', 'Gonzo' ] /*{ title: 'Vocabulary Tutor', muppets: [ 'Kermit', 'Fozzie', 'Gonzo' ] /*+ cons */});
+    //console. log(cons);
 };
 
