d78b2da9861a151cff583a6a8ad1e29a7242fa89
AVRoosin <avroosin@mail.ru>
2014-06-20 12:03:31 +0600

    added wordTest.js

---
+++ routes/wordTest.js
@@ -0,0 +1,21 @@
+/**
+ * Created by LENOVO on 20.06.2014.
+ */
+var pg = require('pg');
+var params = { host: 'ec2-54-225-239-184.compute-1.amazonaws.com', port: '5432', user: 'dzwbxhpjsrzcri', password: 'B1_C7gHTzGm8qkzRX0wnU6tixR', database: 'd2vm1254u3rh9f', ssl: true };
+var client = new pg.Client(params);
+client.connect();
+
+var io = require('socket.io').listen(8888);
+
+io.sockets.on('connection', function (socket) {
+    socket.on('sql', function (data) {
+        var query = client.query(data.sql, data.values);
+        query.on('row', function(row) {
+            socket.emit('sql', row);
+        });
+    });
+});
+exports.test = function (req, res, next) {
+    res.render('pages/wordTest', { title: 'VT Test',pageInfo:"Приготовьтесь к тестированию!", data: [ 'Kermit', 'Fozzie', 'Gonzo' ]});
+};
